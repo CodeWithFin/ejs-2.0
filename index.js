61d66be4c9231ed44710fe832371cbd70abb096f
Finley <finley.mwachia12@gmail.com>
2024-06-05 10:00:30 +0300

    assignment done

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+const app = express();
+const port = 3000;
+const d = new Date();
+var day = d.getDay();
+var motivationalQuote = ""
+
+if (day > 0 && day < 6){
+    motivationalQuote = "Hey, It is A WeekDay , It is Time to Work Hard!!";
+}
+else{
+    motivationalQuote = "Hey, It is A WeekDay , It is Time to Have Fun";
+}
+app.get("/",(req,res)=>{
+res.render("index.ejs",
+    {motivationalQuote}
+);
+});
+
+app.listen(port,()=>{
+    console.log(`Server is Listening on port ${port}`);
+});
+
+
+
+
+
+
+
+
+// console.log(day);
+
